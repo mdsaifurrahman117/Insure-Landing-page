e0f88748eb0193beea0cbf0ffd8dc31277cad0da
mdsaifurrahman00 <saifurrahmansaif954@gmail.com>
2024-04-10 04:08:57 +0600

    initial upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// alert ("Welcome")
+function show_sidebar() {
+            const sidebar = document.querySelector (".sidebar")
+            sidebar.style.display = "flex";
+}
+function hide_sidebar() {
+            const sidebar = document.querySelector (".sidebar")
+            sidebar.style.display = "none"
+}
